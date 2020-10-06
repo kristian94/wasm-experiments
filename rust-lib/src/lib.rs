4b519d5d40e574710937426b7d4f4e10861861df
kristian <kristian.o.g.nielsen@gmail.com>
2020-10-06 17:26:38 +0200

    is_prime added (pretty useless right now, needs tweaking)

--- rust-lib/src/lib.rs
+++ rust-lib/src/lib.rs
@@ -18,3 +18,16 @@ pub fn fib(n: u32) -> u32 {
         _ => fib(n - 1) + fib(n - 2),
     }
 }
+
+#[wasm_bindgen]
+pub fn is_prime(n: u32) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    for a in 2..n {
+        if n % a == 0 {
+            return false; // if it is not the last statement you need to use `return`
+        }
+    }
+    true // last value to return
+}
